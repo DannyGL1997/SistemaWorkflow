bbdb336fc04f1fb909081689d671ce0c77d6eee3
DannyGL1997 <dannygomezloayza@gmail.com>
2023-11-04 10:57:18 -0500

    Add files via upload

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,5 @@
+<?php return array (
+  'customer-project' => 'App\\Http\\Livewire\\CustomerProject',
+  'customerdata' => 'App\\Http\\Livewire\\Customerdata',
+  'test' => 'App\\Http\\Livewire\\Test',
+);
